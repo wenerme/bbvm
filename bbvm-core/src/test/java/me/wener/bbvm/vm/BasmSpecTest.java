936987c951fcd2ff64fbfed9f89c5d6a835b443b
wener <wenermail@gmail.com>
2015-12-18 17:49:47 +0800

    Add file basic graph basm test.

--- bbvm-core/src/test/java/me/wener/bbvm/vm/BasmSpecTest.java
+++ bbvm-core/src/test/java/me/wener/bbvm/vm/BasmSpecTest.java
@@ -14,8 +14,32 @@ import java.nio.file.Paths;
 public class BasmSpecTest {
     @Test
     public void in() throws IOException, ParseException {
+        doTest("../bbvm-test/case/in");
+    }
+
+    @Test
+    public void out() throws IOException, ParseException {
+        doTest("../bbvm-test/case/out");
+    }
+
+    @Test
+    public void file() throws IOException, ParseException {
+        doTest("../bbvm-test/case/file");
+    }
+
+    @Test
+    public void graph() throws IOException, ParseException {
+        doTest("../bbvm-test/case/graph");
+    }
+
+    @Test
+    public void basic() throws IOException, ParseException {
+        doTest("../bbvm-test/case/basic");
+    }
+
+    private void doTest(String first) throws IOException {
         BasmTester test = new BasmTester();
-        Files.walk(Paths.get("../bbvm-test/case/in"))
+        Files.walk(Paths.get(first))
                 .filter(p -> !p.toFile().isDirectory())
                 .filter(p -> p.toFile().getName().endsWith(".basm"))
                 .forEach(p -> test.init(p.toFile()).run());
